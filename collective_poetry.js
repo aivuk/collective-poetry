febc71384248c47eff8664cb7589f0a26b880cca
Edgar Z. Alvarenga <edgar@ymonad.com>
2014-11-16 22:40:58 -0200

    Adding new word with spacebar

--- collective_poetry.js
+++ collective_poetry.js
@@ -1,7 +1,5 @@
 Poem = new Mongo.Collection("poem");
 
-
-
 Meteor.methods({
     updateWord: function (query, newData) {
         Poem.update(query, newData);
@@ -32,116 +30,120 @@ Meteor.methods({
 
         p.words = nw;
         Poem.update({}, p);
+    },
+    insertLineBreak: function (position) {
+        p = Poem.findOne();
+        var nw = p.words;
+
+        for (var i = 0; i < nw.length; ++i) {
+            if(nw[i].pos >= position) {
+                nw[i].pos += 1;
+            }
+        }
+
+        nw.push({"lineBreak": true, "pos": position});
+        p.words = nw;
+        Poem.update({}, p);
     }
 
 });
-
-function poemContent() {
-    pw = Poem.findOne({}).words.sort(function (a,b) { return a.pos - b.pos; });
-    return pw;
-}
 
 Router.route('/poem', function () {
+  poem = Poem.findOne({});
 
-  Session.set("poem_id", Poem.findOne()._id);
-
-  Template.hello.helpers({
-    poem: function () {
-      pw = poemContent();
-      return pw;
-    },
-  });
-
-  Template.hello.events({
-    'click .word-text': function (event) {
-        var wordDOM = $(event.currentTarget);
-        var wordEditDOM = wordDOM.next();
-        wordDOM.toggle();
-        wordEditDOM.toggle();
-        $("input", wordEditDOM).css('width', wordDOM.width() + 5);
-        $("input", wordEditDOM).focus();
-    },
-    'keypress .word-input': function (event) {
-        if (event.which == 13 || event.which == 32) {
-            newWord = $(event.target).val();
-            if (newWord == '') {
-                Meteor.call("removeWord", this.pos);
-            } else {
-                this.word = newWord;
-                Meteor.call("updateWord", {"words.pos": this.pos}, {$set: {"words.$.word": newWord}});
-            }
-            var wordDOM = $(event.currentTarget).parent();
-            var wordEditDOM = wordDOM.prev();
-            wordDOM.toggle();
-            wordEditDOM.toggle();
-        } else {
-            console.log("E");
-            var wordDOM = $(event.currentTarget).parent();
-            $("input", wordDOM).css('width', $("input", wordDOM).val().length*15);
-            console.log(wordEditDOM.width());
-        }
-    },
-    'click .space': function (event) {
-        Meteor.call("insertWord", "", this.pos)
-//        console.log(this.pos);
-    },
-    'click .line-break': function (event) {
-        pw = poemContent();
-        console.log(pw[pw.length -1]);
-    }
-  });
-
-  Template.hello.rendered = function () {
-  }
-
-  Template.word.rendered = function () {
-      console.log(this.data);
-      if (this.data.word == '') {
-        console.log("SIM");
-        var wordDOM = $(".word-text", this.firstNode);
-        var wordEditDOM = $(".word-edit", this.firstNode);
-        wordDOM.toggle();
-        wordEditDOM.toggle();
-        $("input", wordEditDOM).css('width', 15);
-
-      }
-  }
-
-  Template.word.helpers({
-      wordText: function () {
-        return this.word;
-      }
-  });
-
-  insertWord = function (word, position) {
-      Meteor.call("insertWord", word, position);
-  }
-
-  this.layout('hello');
+  this.render('poem');
 
 });
 
-/*
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    var editPosition = 0;
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Template.poem.helpers({
+        'poem': function () {
+            return poemContent();
+        },
+        'editPosition': function() {
+            return editPosition;
+        }
+    })
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+    Template.poem.events({
+       'click .word-text': function (event) {
+          Session.set("editWord", this.pos);
+          Meteor.flush();
+          var wordDOM = $(event.currentTarget);
+          var wordEditDOM = wordDOM.next();
+          wordDOM.toggle();
+          wordEditDOM.toggle();
+          $("input", wordEditDOM).css('width', wordDOM.width() + 5);
+          $("input", wordEditDOM).focus();
+      },
+      'keypress .word-input': function (event) {
+          if (event.which == 13 || event.which == 32) {
+              newWord = $(event.target).val();
+              if (newWord == '') {
+                  Meteor.call("removeWord", this.pos);
+              } else {
+                  this.word = newWord;
+                  Meteor.call("updateWord", {"words.pos": this.pos}, {$set: {"words.$.word": newWord}});
+                  if (event.which == 32) {
+                      Meteor.call("insertWord", "", this.pos + 1);
+                      Session.set("editWord", this.pos + 1);
+                      return;
+                  }
+              }
+              Session.set("editWord", 0);
+          }
+      },
+      'click .space': function (event) {
+          Session.set("editWord", this.pos);
+          Meteor.call("insertWord", "", this.pos)
+      },
+      'click .line-break': function (event) {
+          Meteor.call("insertLineBreak", this.pos);
+          Meteor.call("insertWord", "", this.pos + 1);
+          Session.set("editWord", this.pos + 1);
+      }
+    });
+
+    Template.poem.rendered = function () {
     }
-  });
+
+    Template.word.rendered = function () {
+    }
+
+    Template.wordEdit.rendered = function () {
+        this.$("input").css('width', (this.$("input").val().length + 1)*15);
+        this.$("input").focus();
+    }
+
+    Template.wordEdit.events({
+        'keypress input': function (event) {
+            console.log(event.currentTarget);
+            var numChars = $(event.currentTarget).val().length + 1;
+            $(event.currentTarget).css('width', numChars*15);
+        }
+    });
+
+    Template.word.helpers({
+        wordText: function () {
+          return this.word;
+        },
+        editMode: function () {
+          return this.pos == Session.get("editWord");
+        }
+    });
+
+    function poemContent() {
+        pw = Poem.findOne({}).words.sort(function (a,b) { return a.pos - b.pos; });
+        return pw;
+    }
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-} */
+}
